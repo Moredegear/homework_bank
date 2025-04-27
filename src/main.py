dd6f4808cbf0b9969f9f1e07a9d4e6f8b8027623
Юлия Лёвина <ulialevina@192.168.1.7>
2025-04-27 22:24:48 +0300

    fics main.py

--- src/main.py
+++ src/main.py
@@ -7,5 +7,4 @@ if __name__ == "__main__":
     print(get_mask_account("73654108430135874305"))
     print(get_date("2024-03-11T02:26:18.671407"))
     print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Счет 73654108430135874305"
-                            ))
+    print(mask_account_card("Счет 73654108430135874305"))
