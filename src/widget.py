05a5685fd5efb00bb90cb705dea340f2faeeeabd
Юлия Лёвина <ulialevina@192.168.1.7>
2025-04-27 21:40:52 +0300

    make functions and testing then

---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from src.masks import get_mask_card_number
+from src.masks import  get_mask_account
+from typing import Union
+
+def mask_account_card(card: Union[str]) -> Union[str]:
+    card = str(card)
+    card_list = card.split(" ")
+    result_list = []
+    for i in card_list:
+        if i.isdigit():
+            if len(i) == 12:
+                i = get_mask_card_number(i)
+                result_list.append(i)
+            else:
+                i = get_mask_account(i)
+                result_list.append(i)
+        else:
+            result_list.append(i)
+    result = " ".join(result_list)
+    return result
+def get_date(date: Union[str]) -> Union[str]:
+    result_list = []
+    for i in range(len(date)):
+        if date[i] != "T":
+            result_list.append(date[i])
+        else:
+            break
+    result_strinr = ''.join(result_list)
+    result_list = result_strinr.split("-")
+    reversed_result_list = result_list[::-1]
+    result = ".".join(reversed_result_list)
+    return result
+
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,6 +1,15 @@
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
-card = 1234123412341234
-account = 12344567789434485836456
-print(get_mask_card_number(card))
-print(get_mask_account(account))
+from src.widget import mask_account_card
+from src.widget import get_date
+
+date = "2024-03-11T02:26:18.671407"
+card_1 = 1234123412341234
+account_1 = 12344567789434485836456
+card_2 = "Visa Platinum 7000792289606361"
+account_2 = "Счет 73654108430135874305"
+print(get_mask_card_number(card_1))
+print(get_mask_account(account_1))
+print(mask_account_card(account_2))
+print(mask_account_card(card_2))
+print(get_date(date))
