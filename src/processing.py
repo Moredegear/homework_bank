02b858d7fde707a1a25f3c5cdb3c5af608befb2b
ulialevina <evseevdanil41@gmail.com>
2025-05-09 16:22:39 +0300

    add test for processing and fixs functiln

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,14 @@
-def filter_by_state(data_list: list, state: str = "EXECUTED") -> list:
+from typing import Union
+
+def filter_by_state(data_list: list, state: str = "EXECUTED") -> Union[list, str]:
     """функция фильтрования данных по их состоянию"""
+    if data_list == []:
+        return "Список пусой"
+    for i in data_list:
+        if "state" in i:
+            continue
+        else:
+            return "В списке нет ключа 'state'"
     result = []
     for d in data_list:
         if d["state"] == state:
@@ -11,5 +20,10 @@ def filter_by_state(data_list: list, state: str = "EXECUTED") -> list:
 
 def sort_by_date(data_list: list, reverse: bool = True) -> list:
     """функция сорировки данных по дате"""
+    for i in data_list:
+        if 'date' in i:
+            continue
+        else:
+            return "В списке не указаны даты"
     sorted_data_list = sorted(data_list, key=lambda d: d["date"], reverse=reverse)
     return sorted_data_list
