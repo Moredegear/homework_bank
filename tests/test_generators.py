b413470d06360dc369617a9e3acdff14d13edf5a
ulialevina <evseevdanil41@gmail.com>
2025-05-24 16:50:43 +0300

    add tests for generation and fixc generetion

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -3,3 +3,136 @@ from src.generators import transaction_descriptions
 from src.generators import card_number_generator
 
 
+transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    },
+]
+transactions_fixc = [
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    },
+]
+
+
+def test_filter_by_currency():
+    expected_1 = {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
+    expected_2 = {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+    expected_3 = {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    }
+    expected_4 = "Нет вводных данных"
+    expected_5 = "Нет транзакций в заданной валюте"
+    result = filter_by_currency(transactions)
+    result_3 = filter_by_currency(transactions, currency="RUB")
+    result_4 = filter_by_currency([])
+    result_5 = filter_by_currency(transactions_fixc)
+    assert next(result) == expected_1
+    assert next(result) == expected_2
+    assert next(result_3) == expected_3
+    assert next(result_4) == expected_4
+    assert next(result_5) == expected_5
+
+
+def test_transaction_descriptions():
+    expected_1 = "Перевод организации"
+    expected_2 = "Перевод со счета на счет"
+    expected_3 = "Перевод со счета на счет"
+    expected_4 = "Нет входных данных"
+    result = transaction_descriptions(transactions)
+    result_4 = transaction_descriptions([])
+    assert next(result) == expected_1
+    assert next(result) == expected_2
+    assert next(result) == expected_3
+    assert next(result_4) == expected_4
+
+
+def test_card_number_generator():
+    expected_1 = ["0000 0000 0000 0001"]
+    expected_2 = ["0000 0000 0000 0007"]
+    expected_3 = ["0000 0000 0012 3456", "0000 0000 0012 3457", "0000 0000 0012 3458"]
+    result = card_number_generator(1)
+    result_2 = card_number_generator(7)
+    result_3 = card_number_generator(123456, 123458)
+    assert next(result) == expected_1
+    assert next(result_2) == expected_2
+    assert next(result_3) == expected_3
