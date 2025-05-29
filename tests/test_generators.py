ed4331de59530984564999696a65f1c6f88cf761
ulialevina <evseevdanil41@gmail.com>
2025-05-29 14:27:45 +0300

    fixc tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -51,33 +51,16 @@ transactions = [
         "to": "Счет 14211924144426031657",
     },
 ]
-transactions_fixc = [
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
+
 
 @pytest.fixture
 def currency():
     return transactions
+
+
 def test_filter_by_currency_2(currency):
     result = []
-    data =filter_by_currency(currency)
+    data = filter_by_currency(currency)
     for _ in range(2):
         result.append(next(data))
     assert result == [{
@@ -89,16 +72,16 @@ def test_filter_by_currency_2(currency):
         "from": "Счет 75106830613657916952",
         "to": "Счет 11776614605963066702",
     },
-        {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
+     {
+     "id": 142264268,
+     "state": "EXECUTED",
+     "date": "2019-04-04T23:20:05.206878",
+     "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+     "description": "Перевод со счета на счет",
+     "from": "Счет 19708645243227258542",
+     "to": "Счет 75651667383060284188",
     }]
-    assert next(filter_by_currency(currency,currency="EUR")) == "Нет транзакций в заданной валюте"
+    assert next(filter_by_currency(currency, currency="EUR")) == "Нет транзакций в заданной валюте"
     assert next(filter_by_currency([])) == "Нет вводных данных"
     assert next(filter_by_currency(currency, currency="RUB")) == {
         "id": 873106923,
