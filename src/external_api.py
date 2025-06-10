71c25a0323c5ce543e869e05a406a2bc227a6898
ulialevina <evseevdanil41@gmail.com>
2025-06-10 11:05:32 +0300

    add loger in utils function

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,15 @@
 import requests
 import os
 from dotenv import load_dotenv
+import logging
+
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('logs.log')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -8,9 +17,11 @@ API_KEY = os.getenv("API_KEY")
 
 def conversion(amount: float, code: str) -> float:
     """функция конвертации валют в рубли через запрос API"""
+    logger.debug("создаем параметры API-запроса")
     url = "https://api.apilayer.com/exchangerates_data/convert"
     payload = {"amount": amount, "from": "RUB", "to": code}
     headers = {"apikey": API_KEY}
+    logger.debug("делаем запрос, получаем конвертируемую в рубли сумму и возвращаем её")
     response = requests.get(url, headers=headers, params=payload)
     result = response.json()
     amount = result
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,15 +1,1 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
-from src.widget import mask_account_card
-from src.widget import get_date
 
-date = "2024-03-11T02:26:18.671407"
-card_1 = 1234123412341234
-account_1 = 12344567789434485836456
-card_2 = "Visa Platinum 7000792289606361"
-account_2 = "Счет 73654108430135874305"
-print(get_mask_card_number(card_1))
-print(get_mask_account(account_1))
-print(mask_account_card(account_2))
-print(mask_account_card(card_2))
-print(get_date(date))
